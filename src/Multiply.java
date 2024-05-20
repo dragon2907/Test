f00c3c23093fc44357a5bee265bd75f9bfe204b4
Alex <gordeev29071996@gmail.com>
2024-05-20 15:18:23 +0400

    1

---
+++ src/Multiply.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Multiply {
+    public  static void main(String[] args)
+    {
+        double a,b;
+        Scanner in =new Scanner(System.in);
+        System.out.print("a = "); a = in.nextDouble();
+        System.out.print("b= " ); b = in.nextDouble();
+        double p = 2*(a+b);
+        System.out.println(p);
+        in.close();
+    }
+
+}
